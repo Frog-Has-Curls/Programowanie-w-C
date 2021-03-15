8b9694dcd1dd1249555d8a4f05bc5c20b26f1881
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-03-15 11:16:19 +0100

    Create Zadanie4-bmi.c

---
+++ Zadanie4-bmi.c
@@ -0,0 +1,7 @@
+/* Napisz program, który wylicza BMI
+
+program pyta użytkownika o wzrost i wagę
+wypisuje BMI z dokładnością do części dziesiętnej
+https://pl.wikipedia.org/wiki/Wska%C5%BAnik_masy_cia%C5%82a
+*/
+
