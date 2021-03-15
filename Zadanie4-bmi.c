7ec78bbc61b88471d3081dc7a58b7f07cf0f8d01
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-03-15 11:28:48 +0100

    Update Zadanie4-bmi.c

--- Zadanie4-bmi.c
+++ Zadanie4-bmi.c
@@ -1,7 +1,30 @@
-/* Napisz program, który wylicza BMI
+
+    /*
+    Napisz program, który wylicza BMI
 
 program pyta użytkownika o wzrost i wagę
 wypisuje BMI z dokładnością do części dziesiętnej
 https://pl.wikipedia.org/wiki/Wska%C5%BAnik_masy_cia%C5%82a
-*/
+    
+    
+    */
+#include <stdio.h>
+#include <math.h>
 
+int main(void) {
+  float waga,wzrost,y,x;
+    
+  
+  printf("ile wynosi masa twojego ciała w kg?\n");
+    scanf("%f",&waga);
+   
+  printf("ile wynosi twój wzrost w m?\n");
+    scanf("%f",&wzrost);
+
+    y=pow(wzrost,2);
+    x=waga/y;
+
+
+    printf("Twoje BMI wynosi %.1f ",x);
+  return 0;
+}
