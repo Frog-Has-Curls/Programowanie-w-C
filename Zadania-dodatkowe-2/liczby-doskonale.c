9fac67893ca7e3b9154ada4299f59ed4ee7c3fca
Frog-Has-Curls <shin/pei@wp.pl>
2021-04-06 15:31:51 +0200

    Added folder Zadania-dodatkowe-2

---
+++ Zadania-dodatkowe-2/liczby-doskonale.c
@@ -0,0 +1,1 @@
+/*Napisz program, który w zadanym przedziale wypisze wszystkie liczby doskonałe. Liczba doskonała to taka liczba, która jest sumą swoich dzielników całkowitych. Np. 6 ma dzielniki 1,2,3 - ich suma 1+2+3 = 6. Takich liczb jest bardzo niewiele. */
