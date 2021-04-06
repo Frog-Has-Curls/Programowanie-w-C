d8ef81bb626530adbb3f18abe941b14a9d0f3145
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-04-06 20:54:20 +0200

    Update liczby-doskonale.c

--- Zadania-dodatkowe-2/liczby-doskonale.c
+++ Zadania-dodatkowe-2/liczby-doskonale.c
@@ -1,1 +1,5 @@
-/*Napisz program, który w zadanym przedziale wypisze wszystkie liczby doskonałe. Liczba doskonała to taka liczba, która jest sumą swoich dzielników całkowitych. Np. 6 ma dzielniki 1,2,3 - ich suma 1+2+3 = 6. Takich liczb jest bardzo niewiele. */
+/*Napisz program, który w zadanym przedziale wypisze wszystkie liczby doskonałe. 
+Liczba doskonała to taka liczba, która jest sumą swoich dzielników całkowitych. 
+Np. 6 ma dzielniki 1,2,3 - ich suma 1+2+3 = 6. Takich liczb jest bardzo niewiele. */
+
+
