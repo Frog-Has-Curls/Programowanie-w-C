dfadd8409f3f4fb21bd06e975cbb492440062593
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-04-10 23:28:14 +0200

    Update inicjaly.c

--- Zadania-dodatkowe-2/inicjaly.c
+++ Zadania-dodatkowe-2/inicjaly.c
@@ -1,11 +1,14 @@
 /*Napisz program który pyta osobno o imię i nazwisko, a następnie wypisuje inicjały.
 Trudniejsze: Dodaj funkcję, która podaje ile liter ma nazwisko.*/
 
+#include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void){
 
 char imie[20], nazwisko[20];
+int count = 0,i;
 
 printf("Podaj imię \n");
 scanf("%s", imie);
@@ -13,6 +16,15 @@ printf("Podaj nazwisko \n");
 scanf("%s", nazwisko);
 
 printf("Imicjały to %c.%c \n",imie[0],nazwisko[0]);
+
+
+  for (i = 0; nazwisko[i] !='\0'; i++) {
+    if (isalpha(nazwisko[i])) {
+      count++;
+        }
+    }
+    printf("%d\n", count);
+
+
   return 0;
 }
-
