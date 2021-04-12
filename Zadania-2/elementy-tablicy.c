4b5638c27bb6011c679200e69381c7ab74d581f8
Frog-Has-Curls <64136125+Frog-Has-Curls@users.noreply.github.com>
2021-04-12 10:57:51 +0200

    Update elementy-tablicy.c

--- Zadania-2/elementy-tablicy.c
+++ Zadania-2/elementy-tablicy.c
@@ -8,19 +8,15 @@ Wypisz elementy tej tablicy. Jakie wartości wypisano?*/
 #include<stdio.h>
 int main(void){
 
-
+int i;
 int tab[5];
 printf("podaj 5 liczb do tablicy oddzielone przecinkiem\n");
 scanf("%d,%d,%d,%d,%d",&tab[0],&tab[1],&tab[2],&tab[3],&tab[4]);
 
-printf("pierwszy element tablicy to %d \n", tab[0]);
-printf("drugi element tablicy to %d \n", tab[1]);
-printf("trzeci element tablicy to %d \n", tab[2]);
-printf("czwarty element tablicy to %d \n", tab[3]);
-printf("piąty element tablicy to %d \n", tab[4]);
+
+for(i=0;i<=4;i++){
+printf("%d element tablicy to %d \n",i+1, tab[i]);
+}
 
 return 0;
 }
-
-
-//przyznam, nie bardzo zrozumiałam to zadanie
